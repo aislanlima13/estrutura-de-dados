d9166c86d681ed626073b95875a02b6576aeeb7d
aislanlima13 <aislan.lima04@gmail.com>
2020-10-28 20:25:27 -0300

    Adicionando função para trocar último da pilha pelo primeiro item da pilha

--- src/pilhas/Pilha.java
+++ src/pilhas/Pilha.java
@@ -19,6 +19,15 @@ public class Pilha {
         itens[topo] = valor;
         topo ++;
     }
+
+    public void trocaElementos() {
+        /**String[] aux = {"a", "b"}; **/
+
+        double[] aux = new double[tam_max];
+        aux[0] = itens[topo - 1];
+        itens[topo - 1] = itens[0];
+        itens[0] = aux[0];
+    }
     
     public void pop(){
         topo --;
